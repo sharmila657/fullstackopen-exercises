29d36dc92a6e23f1b72a86e2da6fb67c9f27d55b
sharmila675 <sharmilajirel8@gmail.com>
2023-11-30 15:10:56 +0545

    exercise 1.6 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,37 @@
 import { useState } from 'react'
+import MyButton from './components/Button'
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const incrementgood = () => {
+    setGood(good + 6)
+  }
+
+  const incrementneutral = () => {
+    setNeutral(neutral + 2)
+  }
+
+  const incrementbad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      
+      <MyButton someFunction={incrementgood} text={"good"} />
+      <MyButton someFunction={incrementneutral} text={"neutral"} />
+      <MyButton someFunction = {incrementbad} text = {"bad"} />
+      
+      <h1>statistics</h1>
+
+      good {good} <br/>
+      neutral {neutral} <br />
+      bad {bad} 
+      
     </div>
   )
 }
---
+++ part1/unicafe/src/components/Button.jsx
@@ -0,0 +1,6 @@
+const MyButton = (props) => {
+    return <button onClick={props.someFunction}>{props.text}</button>;
+  };
+  
+  export default MyButton;
+  
