3c77f779bcec06cc1c648f1c7d9aa2967707222b
sharmila675 <sharmilajirel8@gmail.com>
2023-12-04 12:14:47 +0545

    completed upto 2.1

---
+++ part2/course-info/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = ({courseName}) => {
+    return(
+      <h1>
+      {courseName}
+
+      </h1>
+    )
+  }
+  export default Header;
---
+++ part2/course-info/src/Components/Part.jsx
@@ -0,0 +1,6 @@
+const Part = ({part}) => {
+    return(
+      <p> {part.name} {part.exercises}</p>
+    )
+  }
+  export default Part;
