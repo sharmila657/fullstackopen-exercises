100a7f15be55b344cf6c3c8a2871f1ac309c9009
sharmila675 <sharmilajirel8@gmail.com>
2023-12-04 13:29:43 +0545

    part2  exercise 2.3 completed

--- part2/course-info/src/Components/Total.jsx
+++ part2/course-info/src/Components/Total.jsx
@@ -1,6 +1,8 @@
 const Total = ({ parts }) => {
-    const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+    
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
 
     return <p>Total exercises: {totalExercises}</p>;
   }
-  export default Total;
+export default Total;
+  
