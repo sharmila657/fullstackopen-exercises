3380eabc1e38857122015a9e980b84355f46f1e6
sharmila675 <sharmilajirel8@gmail.com>
2023-12-06 11:12:58 +0545

    unicafe changes in states

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,37 +3,27 @@ import MyButton from './components/Button'
 import Header from './components/Header'
 import Statistics from './components/Statistics'
 
-const App = () => {
+const App = () => {  
   const title1 = 'give feedback'
   const title2 = 'statistics'
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  // const increment = (state,setState) =>() => {
-  //   setState(state + 1)
-  // }
-
-  const addGood = (good,setGood) => () => {
-    setGood(good + 6);
+ 
+  const increment = (state, setState)=> () => {
+    setState(state + 1);
   }
-  const addNeutral = (neutral, setNeutral)=> () => {
-    setNeutral(neutral + 2);
-  }
-  const addBad = (bad, setBad)=> () => {
-    setBad(bad + 1);
-  }
-  
+ 
   let check = good || neutral || bad
 
 
   return (
     <div>
       <Header title1 = {title1} />
-      <MyButton someFunction= {addGood(good,setGood)} text={"good"} />
-      <MyButton someFunction= {addNeutral(neutral,setNeutral)} text={"neutral"} />
-      <MyButton someFunction = {addBad(bad,setBad)} text = {"bad"} />
+      <MyButton someFunction= {increment(good,setGood)} text={"good"} />
+      <MyButton someFunction= {increment(neutral,setNeutral)} text={"neutral"} />
+      <MyButton someFunction = {increment(bad,setBad)} text = {"bad"} />
       
       <Header title2={title2} />
       {
