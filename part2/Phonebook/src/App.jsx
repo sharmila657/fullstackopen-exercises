1a14a1e77d572e8b68d1a53cb9fecec93ac3bd6b
sharmila675 <sharmilajirel8@gmail.com>
2023-12-14 14:44:46 +0545

    ex 2.16 & 2.17

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from "react";
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
@@ -8,33 +7,57 @@ import personsServices from './services/persons'
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('')
-
   const [newVal, setNewVal] = useState('')
   const [searchVal, setSearchVal] = useState('')
+  const [notification, setNotification] = useState('null');
 
   useEffect(() => {
     personsServices.getAll()
       .then((result) => {
       setPersons(result)
+      })
+    .catch (error => {
+  showErrorNotification('Error fetching data from server');
+  console.error('Error fetching data',error)
     })
     },
-   []);
+    []);
+  
+  const showSuccessNotification = (message) => {
+    setNotification({ type: 'success', message });
+    setTimeout(() => {
+      setNotification(null);
+    }, 2000);
+  }
+
+  const showErrorNotification = (message) => {
+    setNotification({ type: 'error', message });
+    setTimeout(() => {
+      setNotification(null);
+    }, 2000);
+  }
 
 
   const addNote = (event) => {
     event.preventDefault();
 
-    // if (persons.some(person => person.name === newName)) {
+    if (persons.some(person => person.name === newName)) {
     //   alert(`${newName} is already added to the phonebook.`);
+    showErrorNotification(`${newName} is already added to the phonebook.`)
   
-    // }
+    }
 
     let newPerson = { name: newName, number: newVal };
 
     personsServices.create(newPerson)
       .then((result) => {
         setPersons([...persons, result.data])
+        showSuccessNotification(`${newPerson.name} added to the phonebook.`)
       })
+      .catch(error => {
+        showErrorNotification('error adding person, Please try again later')
+        console.error('Error adding person:', error)
+    })
     
     
     const existingPerson = persons.find((person) => person.name === newName);
@@ -51,8 +74,15 @@ const App = () => {
         .update(existingPerson.id, updatedPerson)
         .then((updatedPerson) => {
           setPersons(persons.map((person) => (person.id !== updatedPerson.id ? person : updatedPerson)));
+          showSuccessNotification(`${updatedPerson.name}'s number updated successfully'`)
+
         })
         .catch((error) => {
+          if (error.response && error.response.status === 404) {
+            showErrorNotification(`${updatedPerson.name} not found.`)
+          } else {
+            showErrorNotification('Error deleting person')
+          }
           console.error('Error updating person:', error);
         });
     }
@@ -92,8 +122,14 @@ const App = () => {
         .remove(id)
         .then(() => {
           setPersons(persons.filter((person) => person.id !== id));
+          showSuccessNotification(`${personToDelete.name} deleted from Phonebook.`)
         })
         .catch((error) => {
+          if (error.response && error.response.status === 404) {
+            showErrorNotification(`${personToDelete.name} not found.`)
+          } else {
+            showErrorNotification('Error deleting person')
+          }
           console.error('Error deleting person:', error);
         });
     }
@@ -104,9 +140,15 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
 
-  
-      <Filter handleSearch={handleSearch} />
+      {notification && (
+        <div style={{ color: notification.type === 'error' ? 'red' : 'green', border: '1px solid', padding: '10px', marginBottom: '10px' }}>
+          {notification.message}
+        </div>
+      )}
+      
 
+      <Filter handleSearch={handleSearch}/>
+      
       <h3>Add a new</h3>
 
       <PersonForm
@@ -119,7 +161,10 @@ const App = () => {
 
       <h3>Numbers</h3>
 
-      <Persons filteredPersons={filteredPersons} handleDelete={handleDelete} />
+      <Persons
+        filteredPersons={filteredPersons}
+        handleDelete={handleDelete}
+/>
     </div>
   )
 }
