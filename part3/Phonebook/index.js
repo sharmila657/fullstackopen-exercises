a6bb5fce99ad2e9c03b14bddf6cc33058a8917a8
sharmila675 <sharmilajirel8@gmail.com>
2023-12-18 22:20:57 +0545

    part3 ex-3.7

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const morgan = require("morgan")
 
 app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
 
 let persons = [
     
