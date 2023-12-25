d8b001d671228fbd77ab7090358eaa209d4bc1ed
sharmila675 <sharmilajirel8@gmail.com>
2023-12-25 16:59:08 +0545

    ex-3.20 number schema created

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -16,8 +16,18 @@ const personSchema = new mongoose.Schema({
     minlength: 3,
     required: true,
   },
-  number: String,
-})
+  number: {
+        type: String,
+        validate: {
+          validator: function(v) {
+            return /^\d{2}-\d{7}$|^\d{3}-\d{8}$/.test(v);
+          },
+          message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+      }
+    });
+ 
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
