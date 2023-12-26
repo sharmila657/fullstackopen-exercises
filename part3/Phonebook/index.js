e2b1db27ccc7f614e2c672b7a06c5a8eeee1bde8
sharmila675 <sharmilajirel8@gmail.com>
2023-12-26 14:23:08 +0545

    Phonebook: backend error handled

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -23,7 +23,6 @@ const personSchema = new mongoose.Schema({
           validator: function(v) {
             return /^\d{2}-\d{7}$|^\d{3}-\d{8}$/.test(v);
           },
-          message: props => `${props.value} is not a valid phone number!`
         },
         required: [true, 'User phone number required']
       }
