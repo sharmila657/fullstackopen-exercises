4d52e37523f488ac2bcc35f85e4163232b23ac81
sharmila675 <sharmilajirel8@gmail.com>
2023-12-27 23:43:29 +0545

    ex-4.4  for single test

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,7 +2,15 @@ const dummy = (blogs) => {
     return 1;
 
 }
+const totalLikes = (blogs) => {
+    if (blogs.length === 1) {
+      likeCount = blogs[0].likes;
+    }
+  
+    return likeCount;
+  };
   
   module.exports = {
-    dummy
+      dummy,
+      totalLikes
   }
