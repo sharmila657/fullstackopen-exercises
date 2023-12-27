673f99308d45d483a941ee01baed66e5db6eb443
sharmila675 <sharmilajirel8@gmail.com>
2023-12-27 12:18:53 +0545

    ex-4.2 refactor app and index

---
+++ part4/blog-list/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const { url } = require('./utils/config')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Bloglist = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(url)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Bloglist
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Bloglist(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = app;
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,42 +1,6 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const { url, PORT } = require('./utils/config')
-const {info} = require('./utils/logger')
-
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Bloglist = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(url)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Bloglist
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Bloglist(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const app = require('./app')
+const { PORT } = require('./utils/config')
+const { info } = require('./utils/logger')
 
 app.listen(PORT, () => {
   info(`Server running on port ${PORT}`,"logging from index")
