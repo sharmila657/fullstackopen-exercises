286c615a1b52495ddc0bd5784f80e188dd81d595
sharmila675 <sharmilajirel8@gmail.com>
2024-01-07 23:19:09 +0545

    ex-4.19 added new blogs with valid token

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,6 +1,8 @@
 const app = require("express").Router()
 const Bloglist = require("../models/blogSchema")
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
 
 app.get('/', async(request, response) => {
     const blogs = await Bloglist
@@ -20,14 +22,27 @@ app.get('/:id', (request, response,next) => {
     })
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
   
   app.post('/', async(request, response,next) => {
    
     // if (!blog.title || !blog.url) {
     //   response.status(400).json({error:"missing property"}).end()
     // }
+    
     try {
-      const bloguser = await User.findById(request.body.userId);
+      const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+      if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+      }
+      const bloguser = await User.findById(decodedToken.id)
+      // const bloguser = await User.findById(request.body.userId);
       const blog = new Bloglist({
       title: request.body.title,
       author: request.body.author,
