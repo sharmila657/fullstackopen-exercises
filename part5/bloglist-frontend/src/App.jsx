c222dc01ec62436697dfe2168f217c6930e01b84
sharmila675 <sharmilajirel8@gmail.com>
2024-01-17 16:08:38 +0545

    ex-5.2 made login permanent using localstorage and also added logout.

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -5,73 +5,89 @@ import loginServices from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('') 
-  const [user,setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
+    //get data from backend server
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
+    //get user from localstorage if available
+    let myuser = window.localStorage.getItem("user")
+    if (myuser) {
+      setUser(JSON.parse(myuser))
+    }
   }, [])
 
   const handleLogin = async (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
-    let loggedinUser = await loginServices.login({
-      username,
-      password
-    })
-    setUser(loggedinUser)
+        let user = await loginServices.login({
+          username,
+          password
+        })
+      setUser(user)
+      window.localStorage.setItem(
+        'user', JSON.stringify(user)
+    ) 
+    blogService.setToken(user.token)
   }
  
-  const loginForm = () => {
-    return(
-      <div>
-        <h2>Log in to application</h2>
-    <form onSubmit={handleLogin}>
+    const loginForm = () => {
+      return (
         <div>
-          username
-            <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
+          <h2>Log in to application</h2>
+          <form onSubmit={handleLogin}>
+            <div>
+              username
+              <input
+                type="text"
+                value={username}
+                name="Username"
+                onChange={({ target }) => setUsername(target.value)}
+              />
+            </div>
+            <div>
+              password
+              <input
+                type="password"
+                value={password}
+                name="Password"
+                onChange={({ target }) => setPassword(target.value)}
+              />
+            </div>
+            <button type="submit">login</button>
+          </form>
         </div>
-        <div>
-          password
-            <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          </div>
-          <button type="submit">login</button>
-        </form>
-  </div>
-    )
-  }
+      )
+    }
   const blogForm = () => {
+    const handleLogout = () => {
+      window.localStorage.removeItem('user');
+      setUser(null);
+      }
+      return (
+        <div>
+          <h2>blogs</h2>
+          {user.name} logged in
+          <button onClick={handleLogout}>logout</button>
+          <br />
+          <br/>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+
+        </div>
+      )
+    }
     return (
       <div>
-        <h2>blogs</h2>
-        <p>{user.name} logged in</p>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
-        )}
-
+        {user === null ? loginForm() : blogForm()}
       </div>
     )
   }
-  return (
-    <div>
-      {user === null ? loginForm() : blogForm()}
-      
-    
-    </div>
-  )
-}
+
 
 export default App
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,23 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3003/api/blogs/'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = async (blogs) => {
   const response = await axios.get(baseUrl,blogs)
   return response.data;
 }
 
-export default { getAll }
+const create = async newblogs => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.post(baseUrl, newblogs, config)
+  return response.data;
+}
+
+export default { getAll, create, setToken}
