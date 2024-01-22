b7108180e6c0ebefd46f3c0236896b3fa355cd9e
sharmila675 <sharmilajirel8@gmail.com>
2024-01-22 11:40:20 +0545

    ex.5.7 made every blog toggleable with their details.

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,42 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState('')
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const togglAble = () => {
+  setShowDetails(!showDetails)
+}
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={togglAble}>
+          {showDetails ? 'Hide' : 'View'}
+        </button>
+      </div>
+      {showDetails && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            Likes: {blog.likes}{' '}
+            <button onClick={() => console.log('Like button clicked')}>
+              Like
+            </button>
+          </div>
+          <div>{blog.author}</div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Blog;
