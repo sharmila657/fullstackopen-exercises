3688617da899cc512183de10f1873c57d4030b77
sharmila675 <sharmilajirel8@gmail.com>
2024-01-29 17:03:35 +0545

    ex-5.14 tested likes and url while clicking view button.

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import blogService from "../services/blogs";
 
-const Blog = ({ blog, setBlogs,loggedinUser}) => {
+const Blog = ({ blog, setBlogs, loggedinUser }) => {
   const [blogToShow, setBlogToShow] = useState([]);
   const blogStyle = {
     paddingTop: 10,
@@ -36,7 +36,9 @@ const Blog = ({ blog, setBlogs,loggedinUser}) => {
   };
 
   const handleDelete = async (blog) => {
-    const confirmation = window.confirm("Do you really want to remove this blog?");
+    const confirmation = window.confirm(
+      "Do you really want to remove this blog?"
+    );
     if (confirmation) {
       try {
         await blogService.deleteBlog(blog.id);
@@ -50,23 +52,23 @@ const Blog = ({ blog, setBlogs,loggedinUser}) => {
   if (blogToShow.includes(blog.id)) {
     return (
       <div style={blogStyle} className="blog-div">
-          {blog.title}
-          <button
-            onClick={() =>
-              setBlogToShow(blogToShow.filter((id) => id !== blog.id))
-            }
-          >
-            hide
-          </button>
-          <br />
-          {blog.url}
-           <br/>
-             Likes: {blog.likes}{" "}
-             <button onClick={() => handleLikes(blog)}  id="like-blog" className="like-btn">Like</button>
-            <br/>
-          {blog.author}
-          <br/>
-           {blog.user.name}
+        {blog.title}
+        <button
+          onClick={() =>
+            setBlogToShow(blogToShow.filter((id) => id !== blog.id))
+          }
+        >
+          hide
+        </button>
+        <br />
+        url: {blog.url}
+        <br />
+        Likes: {blog.likes}{" "}
+        <button onClick={() => handleLikes(blog)}>Like</button>
+        <br />
+        {blog.author}
+        <br />
+        {blog.user.name}
         <br />
         <div>
           {loggedinUser.username === blog.user.username ? (
@@ -76,16 +78,15 @@ const Blog = ({ blog, setBlogs,loggedinUser}) => {
             >
               Remove
             </button>
-          ):null}
-          </div>
+          ) : null}
+        </div>
       </div>
-    )
+    );
   }
   return (
     <div style={blogStyle} className="blog-div">
       {blog.title} {blog.author}
-      <button
-        onClick={() => setBlogToShow([...blogToShow, blog.id])}>
+      <button onClick={() => setBlogToShow([...blogToShow, blog.id])}>
         view
       </button>
     </div>
@@ -93,5 +94,3 @@ const Blog = ({ blog, setBlogs,loggedinUser}) => {
 };
 
 export default Blog;
-
-
--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,7 +1,8 @@
-import React from "react"
+import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Blog from "./Blog";
+import userEvent from "@testing-library/user-event";
 
 test("renders content", () => {
   const blog = {
@@ -10,16 +11,46 @@ test("renders content", () => {
     url: "https://www.joelonsoftware.com/2000/08/09/the-joel-test-12-steps-to-better-code/",
     likes: 112,
   };
-  
+
   const { container } = render(<Blog blog={blog} />);
   const div = container.querySelector(".blog-div");
-  // const otherdiv = container.querySelector(".blog-details")
-  // console.log("testing",otherdiv)  
-  // screen.debug(otherdiv)
+
   expect(div).toHaveTextContent("The Joel Test: 12 Steps to Better Code");
   expect(div).toHaveTextContent("Joel Spolsky");
   expect(screen.queryByText("likes")).toBeNull();
   expect(screen.queryByText("url")).toBeNull();
 });
 
+test.only("Likes and Url will be shown when toggled", async () => {
+  const blog = {
+    title: "test blog",
+    author: "sharmila",
+    url: "http://test.com",
+    likes: 131,
+    user: {
+      username: "blogger",
+      name: "blog",
+      id: "blog",
+    },
+  };
+  const  newUser = {
+    username: "blogger",
+    name: "blog",
+    id: "blog",
+  };
 
+  const { container } = render(<Blog blog={blog} loggedinUser={newUser} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+  await user.click(button);
+  // screen.debug();
+
+  const div = container.querySelector(".blog-div");
+  expect(div).toHaveTextContent(
+    "http://test.com"
+  );
+  expect(div).toHaveTextContent("131");
+  expect(screen.queryByText("url")).toBeDefined();
+  expect(screen.queryByText("Likes")).toBeDefined();
+});
