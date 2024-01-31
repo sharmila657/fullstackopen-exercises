1eb68b76034be81995a3490f668a8b1216fd14be
sharmila675 <sharmilajirel8@gmail.com>
2024-01-31 23:51:32 +0545

    fixed:notification for deletion.

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import blogService from "../services/blogs";
-
-const Blog = ({ blog, setBlogs, loggedinUser, handleLikes}) => {
+const Blog = ({ blog, setBlogs, loggedinUser, handleLikes,setNotification}) => {
   const [blogToShow, setBlogToShow] = useState([]);
   const blogStyle = {
     paddingTop: 10,
@@ -26,6 +25,10 @@ const Blog = ({ blog, setBlogs, loggedinUser, handleLikes}) => {
       try {
         await blogService.deleteBlog(blog.id);
         setBlogs((blogs) => blogs.filter((item) => item.id !== blog.id));
+        setNotification({message:"Blog deleted successfully!"}); 
+        setTimeout(() => {
+          setNotification(null); 
+        }, 3000);
       } catch (error) {
         console.error("Error deleting blog:", error);
       }
@@ -57,7 +60,7 @@ const Blog = ({ blog, setBlogs, loggedinUser, handleLikes}) => {
           {loggedinUser.username === blog.user.username ? (
             <button
               onClick={() => handleDelete(blog)}
-              style={blogStyle.removebutton}
+              style={blogStyle.removebutton} id="remove"
             >
               Remove
             </button>
