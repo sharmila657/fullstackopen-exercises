53ecd22954d58276245c3a488678fd4a160294f1
sharmila675 <sharmilajirel8@gmail.com>
2024-02-06 17:22:51 +0545

    ex-6.2 already implimented actual functionality.

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
 import reducer from './reducer'
+
 const store = createStore(reducer)
 
 const App = () => {
