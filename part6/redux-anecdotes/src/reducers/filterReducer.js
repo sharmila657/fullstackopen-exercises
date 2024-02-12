3a1a160ba7af7e7dd6ddf99ec5fbc2b227f0a4b3
sharmila675 <sharmilajirel8@gmail.com>
2024-02-12 07:41:52 +0545

    ex-6.11 added redux toolkit in filter reducer

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,19 +1,30 @@
-const filterReducer = (state = "", action) => {
-    switch (action.type) {
-      case "FILTER-ANECDOTE": {
-        return action.payload;
-      }
+// const filterReducer = (state = "", action) => {
+//     switch (action.type) {
+//       case "FILTER-ANECDOTE": {
+//         return action.payload;
+//       }
   
-      default:
-        return state;
-    }
-  };
+//       default:
+//         return state;
+//     }
+//   };
   
-  export const anecdoteFilterFrom = (searchTerm) => {
-    return {
-      type: "FILTER-ANECDOTE",
-      payload: searchTerm,
-    };
-  };
+//   export const anecdoteFilterFrom = (searchTerm) => {
+//     return {
+//       type: "FILTER-ANECDOTE",
+//       payload: searchTerm,
+//     };
+//   };
+import { createSlice } from "@reduxjs/toolkit";
   
-  export default filterReducer;
+const filterReducer = createSlice({
+  name: "filter",
+  initialState: "",
+  reducers: {
+    anecdoteFilterFrom(state, action) {
+      return action.payload;
+    },
+  },
+});
+export const { anecdoteFilterFrom } = filterReducer.actions;
+export default filterReducer.reducer;
