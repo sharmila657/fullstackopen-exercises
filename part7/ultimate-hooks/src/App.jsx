d1357921d78b430a8d02a6467f7c3953f304efa5
sharmila657 <sharmilajirel8@gmail.com>
2024-03-11 10:54:53 +0545

    ex-7.8 ultimate-hook created using baseUrl and useResource hook.

--- part7/ultimate-hooks/src/App.jsx
+++ part7/ultimate-hooks/src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([]);
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then((response) => {
+        setResources(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [baseUrl]);
+
+  const create = (resource) => {
+    axios
+      .post(baseUrl, resource)
+      .then((response) => setResources([...resources, response.data]))
+      .catch((error) => console.log(error));
+  };
+
+  const service = {
+    create,
+  };
+
+  return [resources, service];
+};
+
+const App = () => {
+  const content = useField("text");
+  const name = useField("text");
+  const number = useField("text");
+
+  const [notes, noteService] = useResource("http://localhost:3005/notes");
+  const [persons, personService] = useResource("http://localhost:3005/persons");
+
+  const handleNoteSubmit = (event) => {
+    event.preventDefault();
+    noteService.create({ content: content.value });
+  };
+
+  const handlePersonSubmit = (event) => {
+    event.preventDefault();
+    personService.create({ name: name.value, number: number.value });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content} />
+        <button>create</button>
+      </form>
+      {notes.map((n) => (
+        <p key={n.id}>{n.content}</p>
+      ))}
 
-export default App
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name} /> <br />
+        number <input {...number} />
+        <button>create</button>
+      </form>
+      {persons.map((n) => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
