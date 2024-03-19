c96e68b779357cc54b0e37760f23230c7f139876
sharmila657 <sharmilajirel8@gmail.com>
2024-03-20 00:23:17 +0545

    ex-7.21 completed exercise by styling the application.

--- part7/bloglist/src/components/Notification.jsx
+++ part7/bloglist/src/components/Notification.jsx
@@ -1,16 +1,16 @@
 import { useSelector } from "react-redux";
+import { Alert } from "@mui/material";
+
 const Notification=()=>{
   const notification = useSelector((state)=>state.notifications)
  
-  const style = {
-  border: "solid",
-  padding: 10,
-  boderWidth: 1,
-};
 if (notification === null) {
   return null;
 }
-return <div style={style}>{notification}</div>;
+return(
+<div>
+<Alert severity="success">{notification}</Alert>
+</div>)
 }
 
 export default Notification;
